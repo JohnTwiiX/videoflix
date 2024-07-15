34e36fa10e7db40938176edad97bb64db6a9f0e5
JohnTwiiX <john.fieweger98@gmail.com>
2024-07-15 14:41:00 +0200

    bug fix

--- Backend/videoflix/settings.py
+++ Backend/videoflix/settings.py
@@ -128,7 +128,7 @@ WSGI_APPLICATION = 'videoflix.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
