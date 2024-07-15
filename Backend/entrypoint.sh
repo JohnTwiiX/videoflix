9d7ce9139b595177b92b5d635f30409d970a28e0
JohnTwiiX <john.fieweger98@gmail.com>
2024-07-15 15:27:26 +0200

    bug fix

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -2,17 +2,21 @@
 set -e
 
 echo "Waiting for postgres to connect ..."
-
 while ! nc -z videoflix-db 5432; do
   sleep 0.1
 done
-
 echo "PostgreSQL is active"
+
+# Warten Sie, bis die Django-Anwendung vollständig initialisiert ist
+python manage.py check --deploy  # Dies prüft, ob die App bereit ist
+echo "Django application is ready"
 
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 
+# Start RQ Worker in the Hintergrund
 nohup rq worker --url redis://videoflix-redis:6379/0 &
 
+# Start Gunicorn mit 3 Arbeitern
 gunicorn videoflix.wsgi:application --bind 0.0.0.0:8000 --timeout 300 --workers 3
