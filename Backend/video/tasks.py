fc8de7231479658b84ee151bd3afd8d7a3c37b4a
JohnTwiiX <john.fieweger98@gmail.com>
2024-07-15 13:04:45 +0200

    bug fix signals

--- Backend/video/tasks.py
+++ Backend/video/tasks.py
@@ -1,6 +1,6 @@
 import subprocess
 import os
-
+import concurrent.futures
 
 def convertVideos(source):
     new_file_name_480p = source.video_file.path + '_480p.mp4'
@@ -17,8 +17,8 @@ def convertVideos(source):
             os.path.basename(new_file_name_720p)
         source.save()
 
-
 def convert480p(source):
+    print("In convert480p Funktion")
     new_file_name = source.video_file.path + '_480p.mp4'
     cmd = [
         'ffmpeg',
@@ -30,15 +30,20 @@ def convert480p(source):
         '-strict', '-2',
         new_file_name
     ]
-    run = subprocess.run(cmd, capture_output=True, text=True)
+    print(f"Ausführen von Befehl: {' '.join(cmd)}")
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(subprocess.run, cmd, capture_output=True, text=True)
+        run = future.result()
 
     if run.returncode == 0:
+        print("Konvertierung erfolgreich")
         return True
     else:
         print("Fehler bei der Konvertierung:", run.stderr)
 
-
 def convert720p(source):
+    print("In convert720p Funktion")
     new_file_name = source.video_file.path + '_720p.mp4'
     cmd = [
         'ffmpeg',
@@ -50,9 +55,15 @@ def convert720p(source):
         '-strict', '-2',
         new_file_name
     ]
-    run = subprocess.run(cmd, capture_output=True, text=True)
+    print(f"Ausführen von Befehl: {' '.join(cmd)}")
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(subprocess.run, cmd, capture_output=True, text=True)
+        run = future.result()
 
     if run.returncode == 0:
+        print("Konvertierung erfolgreich")
         return True
     else:
         print("Fehler bei der Konvertierung:", run.stderr)
+
