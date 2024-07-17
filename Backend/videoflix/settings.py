6c20e6c002c454e5d0d9aa3d28c7f8db50f83247
John Fieweger <104139228+JohnTwiiX@users.noreply.github.com>
2024-07-17 13:07:39 +0200

    Update settings.py

--- Backend/videoflix/settings.py
+++ Backend/videoflix/settings.py
@@ -58,9 +58,8 @@ RQ_QUEUES = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
-EMAIL_PORT = 465
+EMAIL_USE_TLS = True 
+EMAIL_PORT = 587  # Standard-Port for TLS - Gmail
 EMAIL_HOST_USER = env("EMAIL_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
 
