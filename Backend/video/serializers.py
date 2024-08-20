6627360571bea3358cb92e76b542bf028c216c07
John TwiiX <104139228+JohnTwiiX@users.noreply.github.com>
2024-08-20 21:42:16 +0200

    Update serializers.py

--- Backend/video/serializers.py
+++ Backend/video/serializers.py
@@ -1,3 +1,6 @@
+from rest_framework import serializers
+from .models import Video
+
 class VideoSerializer(serializers.ModelSerializer):
     cover_file = serializers.SerializerMethodField()
     video_file = serializers.SerializerMethodField()
